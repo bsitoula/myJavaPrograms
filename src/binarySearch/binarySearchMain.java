b784c9639b9cb633dd93e625801654e0099f7ea6
Bibas Sitoula <bibas761@gmail.com>
2017-02-22 22:31:12 -0600

    added the string in print statement

--- src/binarySearch/binarySearchMain.java
+++ src/binarySearch/binarySearchMain.java
@@ -5,7 +5,7 @@ public class binarySearchMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//print statement
-		System.out.println("binary search");
+		System.out.println("binary search from the main function");
 	}
 
 }
